044707f1dd9afb54fd8465d2a5002b0a80b011df
Oscshup <51968074+Oscshup@users.noreply.github.com>
2024-09-30 20:18:30 +0200

    Update

---
+++ P0 project/Assets/Scripts/PanelMark.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class PanelHoverTextMeshPro : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public string hoverText; // The specific text for this panel
+    public TextMeshProUGUI displayText; // Reference to the TextMeshPro element
+
+    // Called when the mouse pointer enters the panel
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        displayText.text = hoverText;
+    }
+
+    // Called when the mouse pointer exits the panel
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        displayText.text = "";
+    }
+}
